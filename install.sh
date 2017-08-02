1684e7a53d8844268f028e3ea4a41fc62037b1df
@ivanprz <ivan@ivanperez.com>
2017-08-02 12:30:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update && brew doctor
 git clone https://github.com/ivanperez/dotfiles.git ~/a/dotfiles
 cd ~/a/dotfiles
 bash ./install_osx.sh
