e000b50a63b34daffb1940506f88440eca060ce8
@ivanprz <ivan@ivanperez.com>
2017-08-03 13:15:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+#XCODE
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing them..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" -v;
+else
+  echo "Xcode CLI tools OK"
+fi
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update && brew doctor
 git clone https://github.com/ivanperez/dotfiles.git ~/a/dotfiles
